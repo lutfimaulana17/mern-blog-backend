3d309cf64f58efc508b246ead742421227ad7c9c
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2020-11-07 08:59:12 +0700

    add dummy endpoint create blog

---
+++ src/controllers/blog.js
@@ -0,0 +1,22 @@
+exports.createBlogPost = (req, res, next) => {
+    const title = req.body.title;
+    // const image = req.body.image;
+    const body = req.body.body;
+
+    const result = {
+        message: 'Create Blog Success',
+        data: {
+            post_id: 1,
+            title: "Title Blog",
+            image: "imagefile.png",
+            body: "Lorem Ipsum Dolar Omet lih",
+            created_at: "12/06/2020",
+            author: {
+                uid: 1,
+                name: "Testing"
+            }
+        }
+    }
+
+    res.status(201).json(result);
+}
