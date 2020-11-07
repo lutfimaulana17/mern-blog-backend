f1f4305db284174bd0c71e94ead35fa05fe62051
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2020-11-07 09:52:21 +0700

    Update add express validator input

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const router = express.Router();
 const blogController = require('../controllers/blog')
 
-router.post('/post', blogController.createBlogPost);
+router.post('/post', [
+    body('title').isLength({min: 5}).withMessage('Input title tidak sesuai'), 
+    body('body').isLength({min: 5}).withMessage('Input body tidak sesuai')], 
+    blogController.createBlogPost);
 
 module.exports = router
