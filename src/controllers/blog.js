25f04e2a916c63e287d693c782475c993c180981
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2020-11-08 11:18:06 +0700

    Create request Get All Blog, Get Blog By Id, Create Blog and Update Blog Data

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -43,3 +43,82 @@ exports.createBlogPost = (req, res, next) => {
         console.log('err :', err)
     });
 }
+
+exports.getAllBlogPost = (req, res, next) => {
+    BlogPost.find()
+    .then(result => {
+        res.status(200).json({
+            message: 'Data Blog Post Berhasil dipanggil',
+            data: result
+        })
+    })
+    .catch(err => {
+        next(err);
+    })
+}
+
+exports.getBlogPostById = (req, res, next) => {
+    const postId = req.params.postId;
+    BlogPost.findById(postId)
+    .then(result => {
+        if (!result) {
+            const error = new Error('Blog Post tidak ditemukan');
+            error.errorStatus = 404;
+            throw error;
+        }
+        res.status(200).json({
+            message: 'Data Blog Post Berhasil Dipanggil',
+            data: result
+        })
+    })
+    .catch(err => {
+        next(err);
+    });
+}
+
+exports.updateBlogPost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const err = new Error('Input value tidak sesuai');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
+
+    if (!req.file) {
+        const err = new Error('Image harus di upload');
+        err.errorStatus = 422;
+        throw err;
+    }
+
+    const title = req.body.title;
+    const image = req.file.path;
+    const body = req.body.body;
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+    .then(post => {
+        if (!post) {
+            const err = new Error('Blog Post tidak ditemukan');
+            err.errorStatus = 404;
+            throw err;
+        }
+
+        post.title = title;
+        post.image = image;
+        post.body = body;
+
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Update Success',
+            data: result
+        })
+    })
+    .catch(err => {
+        next(err);
+    })
+
+}
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -9,4 +9,11 @@ router.post('/post', [
     body('body').isLength({min: 5}).withMessage('Input body tidak sesuai')], 
     blogController.createBlogPost);
 
+router.get('/posts', blogController.getAllBlogPost);  
+router.get('/post/:postId', blogController.getBlogPostById);
+router.put('/post/:postId', [
+    body('title').isLength({min: 5}).withMessage('Input title tidak sesuai'), 
+    body('body').isLength({min: 5}).withMessage('Input body tidak sesuai')], 
+    blogController.updateBlogPost);
+
 module.exports = router
